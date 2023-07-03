604c78a84e0d56ba9ca2f9dd25736689bec3f822
loctd <lodtd.work@gmail.com>
2023-07-03 14:59:49 +0700

    ViewVideo: controls video

--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
 const videoApp = document.querySelector('.app-video');
-const buttonPlay = document.querySelector('.app-button__play');
-const buttonPause = document.querySelector('.app-button__pause');
+const buttonControls = document.querySelector('.app-button__controls');
+const buttonPrev = document.querySelector('.app-button__prev');
+const buttonNext = document.querySelector('.app-button__next');
+const iconButtonControls = buttonControls.children[0].classList;
 
+const handlePlayPause = () => {
+    if(iconButtonControls.contains('fa-play')) {
+        iconButtonControls.remove('fa-play');
+        iconButtonControls.add('fa-pause');
+    }
+    else {
+        iconButtonControls.remove('fa-pause');
+        iconButtonControls.add('fa-play');
+    }
+}
 
-buttonPlay.addEventListener('click', () => {
-    videoApp.play();
+buttonControls.addEventListener('click', () => {
+    buttonControls.classList.toggle('video-play');
+    buttonControls.classList.contains('video-play') ? videoApp.play() : videoApp.pause();
+    handlePlayPause();
 });
 
-buttonPause.addEventListener('click', () => {
-    videoApp.pause();
+buttonNext.addEventListener('click', () => {
+    videoApp.currentTime = videoApp.currentTime + 5;
 });
 
-
+buttonPrev.addEventListener('click', () => {
+    videoApp.currentTime = videoApp.currentTime - 5;
+});
