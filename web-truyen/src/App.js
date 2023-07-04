3e7faad125cdbd2cb8e4a83ce661b99626ed1d45
loctd <lodtd.work@gmail.com>
2023-07-04 11:18:52 +0700

    app: category

--- web-truyen/src/App.js
+++ web-truyen/src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './components/Header.js'
 import Banner from './components/Banner.js'
+import Category from './components/Category.js'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Banner></Banner>
+      <Category></Category>
     </div>
   );
 }
