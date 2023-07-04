236fcd73b5f885587490d0d89b0530234c3f8506
loctd <lodtd.work@gmail.com>
2023-07-04 11:16:27 +0700

    app: banner

--- web-truyen/src/App.js
+++ web-truyen/src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './components/Header.js'
+import Banner from './components/Banner.js'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Banner></Banner>
     </div>
   );
 }
