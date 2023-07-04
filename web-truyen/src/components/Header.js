4dcbe50f521a074f9bb13ac0a3587bb954259cda
loctd <lodtd.work@gmail.com>
2023-07-04 11:13:42 +0700

    app: header

---
+++ web-truyen/src/components/Header.js
@@ -0,0 +1,21 @@
+import logo from '../image/app-logo.jpg';
+
+function Header () {
+    return (
+        <header className="Header">
+          <div className="Header-logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="Header-search">
+            <button type="submit">Search</button>
+            <input className="Header-search__input" placeholder="Tìm kiếm tên truyện" type="search"/>
+          </div>
+          <div className="Header-button">
+            <a href="/#">Đăng nhập</a>
+            <a href="/#">Đăng ký</a>
+          </div>
+        </header>
+    )
+}
+
+export default Header;
