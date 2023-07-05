7acbd282189d070445d77e9ae4d6a71b7292b61e
loctd <lodtd.work@gmail.com>
2023-07-05 11:17:39 +0700

    Banner Slide

---
+++ web-truyen/src/data/data.js
@@ -0,0 +1,83 @@
+import imgCard1 from '../images/image-card-1.png'
+import imgCard2 from '../images/image-card-2.png'
+import imgCard3 from '../images/image-card-3.png'
+import imgCard4 from '../images/image-card-4.png'
+import imgCard5 from '../images/image-card-5.png'
+
+export const topMonth = [
+    {
+        id: 1,
+        name: "Võ Luyện ĐỈnh Phong",
+        image: imgCard1,
+    },
+    {
+        id: 2,
+        name: "Người đẹp",
+        image: imgCard2,
+    },
+    {
+        id: 3,
+        name: "Spiderman",
+        image: imgCard3,
+    },
+    {
+        id: 4,
+        name: "Batman",
+        image: imgCard4,
+    },
+    {
+        id: 5,
+        name: "Phong vân",
+        image: imgCard5,
+    },
+    {
+        id: 6,
+        name: "Võ lâm truyền kỳ",
+        image: imgCard2,
+    },
+    {
+        id: 7,
+        name: "Võ lâm minh chủ",
+        image: imgCard4,
+    },
+    {
+        id: 8,
+        name: "Hùng bá",
+        image: imgCard1,
+    },
+    {
+        id: 9,
+        name: "Hề hề hề",
+        image: imgCard3,
+    },
+    {
+        id: 10,
+        name: "Truyện thú vị",
+        image: imgCard5,
+    },
+    {
+        id: 11,
+        name: "Quỷ tha ma bắt",
+        image: imgCard2,
+    },
+    {
+        id: 12,
+        name: "Zo zo",
+        image: imgCard4,
+    },
+    {
+        id: 13,
+        name: "Siêu sao",
+        image: imgCard1,
+    },
+    {
+        id: 14,
+        name: "Reddot",
+        image: imgCard4,
+    },
+    {
+        id: 15,
+        name: "JSXC",
+        image: imgCard3,
+    },
+]
